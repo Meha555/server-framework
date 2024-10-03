53ce7d1401532b9c25085a615beb4675958e59c9
Meha555 <wdsjhzy@163.com>
2024-10-03 22:58:19 +0800

    feat(address): 封装套接字地址模块

--- include/address.h
+++ include/address.h
@@ -6,60 +6,66 @@
 #include <string>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/un.h>
 
-namespace meha
-{
+namespace meha {
 
 /**
  * @brief 地址基类
  */
-class Address
-{
+class Address {
 public:
-    using ptr = std::shared_ptr<Address>;
+    friend std::ostream &operator<<(std::ostream &os, const Address &addr);
+    using sptr = std::shared_ptr<Address>;
+
+    /**
+     * @brief 便利静态函数：通过外部设置的sockaddr指针创建Address
+     * @param[in] addr sockaddr指针
+     * @param[in] addrlen sockaddr的长度
+     * @return 返回和sockaddr相匹配的Address,失败返回nullptr
+     */
+    static Address::sptr Create(const sockaddr * const addr, const socklen_t addrlen);
 
     virtual ~Address() = default;
 
     // 获取协议族
-    int getFamily() const;
+    sa_family_t family() const;
 
-    // @brief 获取 socket 地址
-    virtual const sockaddr* getAddr() const = 0;
+    // 获取 socket 地址
+    virtual const sockaddr *address() const = 0;
 
-    // @brief 获取地址长度
-    virtual socklen_t getAddrLen() const = 0;
+    // 获取地址长度（字节数）
+    virtual socklen_t addrLen() const = 0;
 
     std::string toString();
 
-    bool operator<(const Address& rhs) const;
-
-    bool operator==(const Address& rhs) const;
-
-    bool operator!=(const Address& rhs) const;
+    // 由于Address可能放在容器中，所以提供一些比较运算符
+    bool operator<(const Address &rhs) const;
+    bool operator==(const Address &rhs) const;
+    bool operator!=(const Address &rhs) const;
 
 protected:
-    virtual std::ostream& insert(std::ostream& os) const;
+    virtual std::ostream &marshal(std::ostream &os) const = 0;
 };
 
 /**
  * @brief IP 地址基类
  */
-class IPAddress : public Address
-{
+class IPAddress : public Address {
 public:
-    using ptr = std::shared_ptr<IPAddress>;
+    using sptr = std::shared_ptr<IPAddress>;
 
     // 获取广播地址
-    virtual IPAddress::ptr broadcastAddress(uint32_t prefix_len) = 0;
+    virtual IPAddress::sptr broadcastAddress(uint32_t mask_bits) = 0;
 
     // 获取网络地址
-    virtual IPAddress::ptr networkAddress(uint32_t prefix_len) = 0;
+    virtual IPAddress::sptr networkAddress(uint32_t mask_bits) = 0;
 
     // 获取子网掩码
-    virtual IPAddress::ptr subnetMask(uint32_t prefix_len) = 0;
+    virtual IPAddress::sptr subnetMask(uint32_t mask_bits) = 0;
 
     // 获取端口号
-    virtual uint32_t getPort() const = 0;
+    virtual uint32_t port() const = 0;
 
     virtual void setPort(uint16_t port) = 0;
 };
@@ -67,35 +73,42 @@ public:
 /**
  * @brief IPv4 地址信息类
  */
-class IPv4Address : public IPAddress
-{
+class IPv4Address : public IPAddress {
 public:
-    using ptr = std::shared_ptr<IPv4Address>;
+    using sptr = std::shared_ptr<IPv4Address>;
 
-    IPv4Address(uint32_t address = INADDR_ANY, uint16_t port = 0);
+    explicit IPv4Address(uint32_t address = INADDR_ANY, uint16_t port = 0);
+    explicit IPv4Address(const sockaddr_in &addr);  // NOTE 这里是浅拷贝
+
+    /**
+     * @brief 根据地址字符串创建对象的辅助函数
+     * @param socket_str marshal格式的字符串
+     * @return sptr 
+     */
+    static sptr Create(const std::string &socket_str);
 
     // 获取 socket 地址
-    const sockaddr* getAddr() const override;
+    const sockaddr *address() const override;
 
-    // 获取地址长度
-    socklen_t getAddrLen() const override;
+    // 获取地址长度（字节数）
+    socklen_t addrLen() const override;
 
     // 获取广播地址
-    IPAddress::ptr broadcastAddress(uint32_t prefix_len) override;
+    IPAddress::sptr broadcastAddress(uint32_t mask_bits) override;
 
     // 获取网络地址
-    IPAddress::ptr networkAddress(uint32_t prefix_len) override;
+    IPAddress::sptr networkAddress(uint32_t mask_bits) override;
 
     // 获取子网掩码
-    IPAddress::ptr subnetMask(uint32_t prefix_len) override;
+    IPAddress::sptr subnetMask(uint32_t mask_bits) override;
 
     // 获取端口号
-    uint32_t getPort() const override;
+    uint32_t port() const override;
 
     void setPort(uint16_t port) override;
 
 protected:
-    std::ostream& insert(std::ostream& os) const override;
+    std::ostream &marshal(std::ostream &os) const override;
 
 private:
     sockaddr_in m_addr;
@@ -104,37 +117,42 @@ private:
 /**
  * @brief IPv6 地址信息类
  */
-class IPv6Address : public IPAddress
-{
+class IPv6Address : public IPAddress {
 public:
-    using ptr = std::shared_ptr<IPv6Address>;
+    using sptr = std::shared_ptr<IPv6Address>;
 
-    IPv6Address();
+    explicit IPv6Address(const char *address = "", uint16_t port = 0);
+    explicit IPv6Address(const sockaddr_in6 &addr);  // NOTE 这里是浅拷贝
 
-    IPv6Address(const char* address = "", uint16_t port = 0);
+    /**
+     * @brief 根据地址字符串创建对象的辅助函数
+     * @param socket_str marshal格式的字符串
+     * @return sptr
+     */
+    static sptr Create(const std::string &socket_str);
 
     // 获取 socket 地址
-    const sockaddr* getAddr() const override;
+    const sockaddr *address() const override;
 
-    // 获取地址长度
-    socklen_t getAddrLen() const override;
+    // 获取地址长度（字节数）
+    socklen_t addrLen() const override;
 
     // 获取广播地址
-    IPAddress::ptr broadcastAddress(uint32_t prefix_len) override;
+    IPAddress::sptr broadcastAddress(uint32_t mask_bits) override;
 
     // 获取网络地址
-    IPAddress::ptr networkAddress(uint32_t prefix_len) override;
+    IPAddress::sptr networkAddress(uint32_t mask_bits) override;
 
     // 获取子网掩码
-    IPAddress::ptr subnetMask(uint32_t prefix_len) override;
+    IPAddress::sptr subnetMask(uint32_t mask_bits) override;
 
     // 获取端口号
-    uint32_t getPort() const override;
+    uint32_t port() const override;
 
     void setPort(uint16_t port) override;
 
 protected:
-    std::ostream& insert(std::ostream& os) const override;
+    std::ostream &marshal(std::ostream &os) const override;
 
 private:
     sockaddr_in6 m_addr;
@@ -143,49 +161,56 @@ private:
 /**
  * @brief Unix 地址信息类
  */
-class UnixAddress : public Address
-{
+class UnixAddress : public Address {
 public:
-    using ptr = std::shared_ptr<UnixAddress>;
+    using sptr = std::shared_ptr<UnixAddress>;
+    // REVIEW 这样->是不算解引用是吗？
+    static constexpr size_t MAX_PATH_LEN = sizeof(((sockaddr_un *)nullptr)->sun_path) - 1;  // -1是因为sun_path是字符数组，末尾终止符\0
 
-    UnixAddress(const std::string& path);
+    explicit UnixAddress(const std::string &path);
 
     // 获取 socket 地址
-    const sockaddr* getAddr() const override;
+    const sockaddr *address() const override;
 
-    // 获取地址长度
-    socklen_t getAddrLen() const override;
+    // 获取地址长度（字节数）
+    socklen_t addrLen() const override;
+
+    // 获取UNIX域套接字地址
+    std::string path() const;
+
+    // 设置UNIX域套接字地址
+    void setPath(const std::string &path);
 
 protected:
-    std::ostream& insert(std::ostream& os) const override;
+    std::ostream &marshal(std::ostream &os) const override;
 
 private:
-    sockaddr_in m_addr;
-    socklen_t m_length;
+    sockaddr_un m_addr;
+    socklen_t m_length;  // 由于UNIX套接字地址长度不固定，因此需要额外的成员来记录地址结构体总长度（字节数）
 };
 
 /**
  * @brief 未知地址信息类
  */
-class UnknowAddress : public Address
-{
+class UnknowAddress : public Address {
 public:
-    using ptr = std::shared_ptr<UnknowAddress>;
+    using sptr = std::shared_ptr<UnknowAddress>;
 
-    UnknowAddress();
+    explicit UnknowAddress(sa_family_t family);
 
     // 获取 socket 地址
-    const sockaddr* getAddr() const override;
+    const sockaddr *address() const override;
 
-    // 获取地址长度
-    socklen_t getAddrLen() const override;
+    // 获取地址长度（字节数）
+    socklen_t addrLen() const override;
 
 protected:
-    std::ostream& insert(std::ostream& os) const override;
+    std::ostream &marshal(std::ostream &os) const override;
 
 private:
     sockaddr m_addr;
-    socklen_t m_length;
 };
 
-} // namespace meha
+inline std::ostream &operator<<(std::ostream &os, const Address &addr) { return addr.marshal(os); }
+
+}  // namespace meha
--- include/scheduler.h
+++ include/scheduler.h
@@ -5,16 +5,41 @@
 #include "thread.h"
 #include "utils.h"
 #include <atomic>
-#include <boost/type_traits.hpp>
 #include <ctime>
 #include <list>
 #include <memory>
+#include <type_traits>
 #include <unistd.h>
 #include <utility>
 #include <variant>
 #include <vector>
 
 namespace meha {
+
+namespace utils {
+namespace internal {
+template <typename Executable>
+struct is_valid_single_type
+{
+    static constexpr bool value = std::is_same_v<std::remove_cv_t<std::remove_reference_t<Executable>>, Fiber::sptr> ||
+                                  std::is_same_v<std::remove_cv_t<std::remove_reference_t<Executable>>, meha::Fiber::FiberFunc> ||
+                                  std::is_pointer<Executable>::value && std::is_same_v<std::remove_pointer_t<Executable>, void (*)(void)> ||
+                                  std::is_invocable_r_v<void, std::decay_t<Executable>>;
+};
+}  // namespace internal
+
+template <typename Executable>
+struct is_vaild_task
+{
+    static constexpr bool value = internal::is_valid_single_type<std::remove_reference_t<Executable>>::value;
+};
+
+template <typename... Types>
+struct is_vaild_task<std::variant<Types...>>  // 为了std::variant的特化
+{
+    static constexpr bool value = (internal::is_valid_single_type<Types>::value || ...);
+};
+}  // namespace utils
 
 /**
  * @brief 协程调度器
@@ -25,190 +50,181 @@ namespace meha {
  * */
 class Scheduler : public noncopyable {
 private:
-  /**
-   * @brief 任务包装类
-   * @note 任务可以是协程对象，也可以是可调用对象，会自动构造为协程
-   * */
-  struct Task {
-    using sptr = std::shared_ptr<Task>;
-    using uptr = std::unique_ptr<Task>;
+    /**
+     * @brief 任务包装类
+     * @note 任务可以是协程对象，也可以是可调用对象，会自动构造为协程
+     * */
+    struct Task
+    {
+        using sptr = std::shared_ptr<Task>;
+        using uptr = std::unique_ptr<Task>;
 
-    // uint64_t id;  // 任务id，用于唯一查找该任务
-    // bool removed; // 任务是否被移除
-    Fiber::sptr handle{nullptr};
-    pid_t thread_id{-1}; // 可选的: 指定执行该任务的线程的id
+        // uint64_t id;  // 任务id，用于唯一查找该任务
+        // bool removed; // 任务是否被移除
+        Fiber::sptr handle{nullptr};
+        pid_t thread_id{-1};  // 可选的: 指定执行该任务的线程的id
 
-    explicit Task() : handle(nullptr), thread_id(-1) {}
-    Task(Fiber::sptr f, pid_t tid)
-        : handle(std::move(f)), thread_id(tid) {
-      ASSERT_FMT(f->isScheduled(), "协程必须参与调度器调度");
-    }
-    Task(Fiber::FiberFunc&& cb, pid_t tid)
-        : handle(std::make_shared<Fiber>(cb, true)), thread_id(tid) {}
+        explicit Task() : handle(nullptr), thread_id(-1) {}
+        Task(Fiber::sptr f, pid_t tid) : handle(std::move(f)), thread_id(tid) { ASSERT_FMT(f->isScheduled(), "协程必须参与调度器调度"); }
+        Task(Fiber::FiberFunc &&cb, pid_t tid) : handle(std::make_shared<Fiber>(cb, true)), thread_id(tid) {}
 
-    // 清除任务
-    void clear() {
-      handle = nullptr;
-      thread_id = -1;
-    }
-
-    template <typename Executable> struct is_vaild_task {
-      static constexpr bool value =
-          boost::is_same<std::remove_reference_t<Executable>,
-                         Fiber::sptr>::value ||
-          boost::is_same<std::remove_reference_t<Executable>,
-                         meha::Fiber::FiberFunc>::value;
+        // 清除任务
+        void clear()
+        {
+            handle = nullptr;
+            thread_id = -1;
+        }
     };
-  };
 
 public:
-  /**
-   * @brief 调度策略 //TODO 还没实现，可以参考一下STL中的适配器
-   * @note 由于估计任务运行时间不知道怎么做，所以只设想了这几种
-   */
-  enum Policy {
-    FCFS, // 先来先服务（默认）
-    PSA,  // 优先级调度
-    MIX,  // 两者混合
-  };
+    /**
+     * @brief 调度策略 //TODO 还没实现，可以参考一下STL中的适配器
+     * @note 由于估计任务运行时间不知道怎么做，所以只设想了这几种
+     */
+    enum Policy
+    {
+        FCFS,  // 先来先服务（默认）
+        PSA,   // 优先级调度
+        MIX,   // 两者混合
+    };
 
-  friend class Fiber;
-  using sptr = std::shared_ptr<Scheduler>;
-  using uptr = std::unique_ptr<Scheduler>;
+    friend class Fiber;
+    using sptr = std::shared_ptr<Scheduler>;
+    using uptr = std::unique_ptr<Scheduler>;
 
-  // 获取当前的调度器
-  static Scheduler *GetCurrent();
-  // 获取当前调度器的调度协程
-  static Fiber *GetSchedulerFiber();
+    // 获取当前的调度器
+    static Scheduler *GetCurrent();
+    // 获取当前调度器的调度协程
+    static Fiber *GetSchedulerFiber();
 
 public:
-  /**
-   * @brief 构造函数
-   * @param pool_size 线程池线程数量
-   * @param use_caller 是否将 Scheduler 所在的线程作为 master fiber
-   * @param name 调度器名称
-   * */
-  explicit Scheduler(size_t pool_size = 1, bool use_caller = true);
-  virtual ~Scheduler();
-  // 开始调度（初始化线程池并启动所有工作线程，等待添加调度任务）
-  void start();
-  // 停止调度（回收调度线程）
-  void stop();
-  virtual bool isStoped() const;
-  bool hasIdleThread() const { return m_idle_thread_count > 0; }
+    /**
+     * @brief 构造函数
+     * @param pool_size 线程池线程数量
+     * @param use_caller 是否将 Scheduler 所在的线程作为 master fiber
+     * @param name 调度器名称
+     * */
+    explicit Scheduler(size_t pool_size = 1, bool use_caller = true);
+    virtual ~Scheduler();
+    // 开始调度（初始化线程池并启动所有工作线程，等待添加调度任务）
+    void start();
+    // 停止调度（回收调度线程）
+    void stop();
+    virtual bool isStoped() const;
+    bool hasIdleThread() const { return m_idle_thread_count > 0; }
 
-  /**
-   * @brief 添加任务 thread-safe
-   * @param Executable 模板类型必须是 std::shared_ptr<meha::Fiber> 或者
-   * std::function<void()>。对于后者会自动构造为协程
-   * @param exec Executable 的实例
-   * @param instant 是否优先调度
-   * @param thread_id
-   * 任务要绑定执行线程的id，-1表示任务不绑定线程（即任意线程均可）
-   * */
-  template <typename Executable>
-  void schedule(Executable &&exec, pid_t thread_id = -1, bool instant = false) {
-    bool need_tickle = false;
+    /**
+     * @brief 添加任务 thread-safe
+     * @param Executable 模板类型必须是 std::shared_ptr<meha::Fiber> 或者
+     * std::function<void()>。对于后者会自动构造为协程
+     * @param exec Executable 的实例
+     * @param instant 是否优先调度
+     * @param thread_id
+     * 任务要绑定执行线程的id，-1表示任务不绑定线程（即任意线程均可）
+     * */
+    template <typename Executable>
+    void schedule(Executable &&exec, pid_t thread_id = -1, bool instant = false)
     {
-      static_assert(
-          Task::is_vaild_task<Executable>::value,
-          "任务类型必须是std::shared_ptr<meha::Fiber>或std::function<void()>"); // FIXME 怎么支持lambda？
-      WriteScopedLock lock(&m_mutex);
-      need_tickle = addTask(std::forward<Executable>(exec), thread_id);
+        bool need_tickle = false;
+        {
+            static_assert(utils::is_vaild_task<Executable>::value,
+                          "任务类型必须是std::shared_ptr<meha::Fiber>或std::function<void()>");  // FIXME 怎么支持lambda？
+            WriteScopedLock lock(&m_mutex);
+            need_tickle = addTask(std::forward<Executable>(exec), thread_id);
+        }
+        // 通知调度器开始新的任务分发调度
+        if (need_tickle) {
+            tickle();
+        }
     }
-    // 通知调度器开始新的任务分发调度
-    if (need_tickle) {
-      tickle();
-    }
-  }
 
-  /**
-   * @brief 添加多个任务 thread-safe
-   * @param begin 单向迭代器
-   * @param end 单向迭代器
-   */
-  template <typename InputIterator>
-  void schedule(InputIterator begin, InputIterator end) {
-    bool need_tickle = false;
+    /**
+     * @brief 添加多个任务 thread-safe
+     * @param begin 单向迭代器
+     * @param end 单向迭代器
+     */
+    template <typename InputIterator>
+    void schedule(InputIterator begin, InputIterator end)
     {
-      WriteScopedLock lock(&m_mutex);
-      while (begin != end) {
-        need_tickle = addTask(std::move(*begin)) || need_tickle;
-        ++begin;
-      }
+        bool need_tickle = false;
+        {
+            WriteScopedLock lock(&m_mutex);
+            while (begin != end) {
+                need_tickle = addTask(std::move(*begin)) || need_tickle;
+                ++begin;
+            }
+        }
+        if (need_tickle) {
+            tickle();
+        }
     }
-    if (need_tickle) {
-      tickle();
-    }
-  }
 
 protected:
-  // 执行调度（调度协程入口）
-  void run();
-  // 通知调度器有新任务
-  virtual void tickle();
-  // 调度器停止时的回调函数
-  virtual bool doStop() const { return isStoped(); }
-  // 调度器空闲时的回调函数
-  virtual void doIdle() {
-    // Idle协程啥也不用做，只是会获取CPU，作用是保持调度器响应用户的schedule添加调度任务的请求
-      std::cerr << ">>> idle id= " << Fiber::GetCurrentID() << ", " << this << '\n';
-    while (!isStoped()) {
-      Fiber::Yield(); // 啥也没做直接yield
-      std::cerr << ">>>>> hahahaha\n";
+    // 执行调度（调度协程入口）
+    void run();
+    // 通知调度器有新任务
+    virtual void tickle();
+    // 调度器停止时的回调函数
+    virtual bool doStop() const { return isStoped(); }
+    // 调度器空闲时的回调函数
+    virtual void doIdle()
+    {
+        // Idle协程啥也不用做，只是会获取CPU，作用是保持调度器响应用户的schedule添加调度任务的请求
+        std::cerr << ">>> idle id= " << Fiber::GetCurrentID() << ", " << this << '\n';
+        while (!isStoped()) {
+            Fiber::Yield();  // 啥也没做直接yield
+            std::cerr << ">>>>> hahahaha\n";
+        }
     }
-  }
 
 private:
-  /**
-   * @brief 添加任务 non-thread-safe
-   * @param Executable 可调用对象
-   * @param exec Executable 的实例
-   * @param thread_id 任务要绑定执行线程的 id //TODO 还没实现
-   * @param instant 是否优先调度
-   * @return 是否是空闲状态下的第一个新任务（//REVIEW
-   * 注意是空闲状态，这是为什么）
-   * */
-  template <typename Executable>
-  bool addTask(Executable &&exec, pid_t thread_id = -1, bool instant = false) {
-    // static_assert(
-    //     Task::is_vaild_task<Executable>::value,
-    //     "任务类型必须是std::shared_ptr<meha::Fiber>或std::function<void()>");
-    bool need_tickle = m_task_list.empty();
-    auto task =
-        std::make_shared<Task>(std::forward<Executable>(exec), thread_id);
-    // 优先调度的任务放在队首
-    if (task->handle) {
-      if (instant)
-        m_task_list.push_front(std::move(task));
-      else
-        m_task_list.push_back(std::move(task));
+    /**
+     * @brief 添加任务 non-thread-safe
+     * @param Executable 可调用对象
+     * @param exec Executable 的实例
+     * @param thread_id 任务要绑定执行线程的 id //TODO 还没实现
+     * @param instant 是否优先调度
+     * @return 是否是空闲状态下的第一个新任务（//REVIEW
+     * 注意是空闲状态，这是为什么）
+     * */
+    template <typename Executable>
+    bool addTask(Executable &&exec, pid_t thread_id = -1, bool instant = false)
+    {
+        static_assert(utils::is_vaild_task<Executable>::value, "任务类型必须是std::shared_ptr<meha::Fiber>或std::function<void()>");
+        bool need_tickle = m_task_list.empty();
+        auto task = std::make_shared<Task>(std::forward<Executable>(exec), thread_id);
+        // 优先调度的任务放在队首
+        if (task->handle) {
+            if (instant)
+                m_task_list.push_front(std::move(task));
+            else
+                m_task_list.push_back(std::move(task));
+        }
+        return need_tickle;
     }
-    return need_tickle;
-  }
 
 protected:
-  // 线程池大小
-  size_t m_thread_pool_size = 0;
-  // 工作线程数量（工作线程里面跑着任务协程）
-  std::atomic_uint64_t m_working_thread_count{0};
-  // 空闲线程数量
-  std::atomic_uint64_t m_idle_thread_count{0};
-  // 是否启动
-  bool m_startting = false;
-  // 是否能停止（成功执行过stop）
-  bool m_stopping = false;
+    // 线程池大小
+    size_t m_thread_pool_size = 0;
+    // 工作线程数量（工作线程里面跑着任务协程）
+    std::atomic_uint64_t m_working_thread_count{0};
+    // 空闲线程数量
+    std::atomic_uint64_t m_idle_thread_count{0};
+    // 是否启动
+    bool m_startting = false;
+    // 是否能停止（成功执行过stop）
+    bool m_stopping = false;
 
 private:
-  mutable RWLock m_mutex;
-  // // 当前执行的任务id
-  // uint64_t m_cur_id;
-  // 调度器所在线程的调度协程，仅在类实例化参数中 use_caller 为 true 时有效
-  Fiber::sptr m_caller_fiber;
-  // 工作线程池
-  std::vector<Thread::uptr> m_thread_pool;
-  // 任务队列（任务由任务协程执行，而任务协程由工作线程中的调度者协程调度）
-  std::list<Task::sptr> m_task_list;
+    mutable RWLock m_mutex;
+    // // 当前执行的任务id
+    // uint64_t m_cur_id;
+    // 调度器所在线程的调度协程，仅在类实例化参数中 use_caller 为 true 时有效
+    Fiber::sptr m_caller_fiber;
+    // 工作线程池
+    std::vector<Thread::uptr> m_thread_pool;
+    // 任务队列（任务由任务协程执行，而任务协程由工作线程中的调度者协程调度）
+    std::list<Task::sptr> m_task_list;
 };
 
-} // namespace meha
+}  // namespace meha
